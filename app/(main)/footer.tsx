4215966737a2b2bde073f9faae7bb7755c44ea84
L1H0n9Jun <lihj@lihj.me>
2024-11-14 10:53:45 +0000

    MOD: Href link to github.

--- app/(main)/footer.tsx
+++ app/(main)/footer.tsx
@@ -1,33 +1,23 @@
-import pack from "@/package.json";
 import { useTranslations } from "next-intl";
 
 export default function Footer() {
   const t = useTranslations("Footer");
-  const version = pack.version;
   return (
     <footer className="mx-auto w-full max-w-5xl">
       <section className="flex flex-col">
         <p className="mt-3 flex gap-1 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
           {t("p_146-598_Findthecodeon")}{" "}
           <a
-            href="https://github.com/hamster1963/nezha-dash"
+            href="https://github.com/L1H0n9Jun/nezha-dash"
             target="_blank"
-            className="cursor-pointer font-normal underline decoration-yellow-500 hover:decoration-yellow-600 transition-colors decoration-2 underline-offset-2 dark:decoration-yellow-500/60 dark:hover:decoration-yellow-500/80"
+            className="cursor-pointer font-normal underline decoration-stone-200 hover:decoration-stone-300 transition-colors decoration-2 underline-offset-2 dark:decoration-stone-800 dark:hover:decoration-stone-700"
           >
             {t("a_303-585_GitHub")}
-          </a>
-          <a
-            href={`https://github.com/hamster1963/nezha-dash/releases/tag/v${version}`}
-            target="_blank"
-            className="cursor-pointer font-normal underline decoration-yellow-500 hover:decoration-yellow-600 transition-colors decoration-2 underline-offset-2 dark:decoration-yellow-500/60 dark:hover:decoration-yellow-500/80"
-          >
-            v{version}
           </a>
         </p>
         <section className="mt-1 flex items-center gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
           {t("section_607-869_2020")}
-          {new Date().getFullYear()}{" "}
-          <a href={"https://buycoffee.top"}>{t("a_800-850_Hamster1963")}</a>
+          {new Date().getFullYear()}
         </section>
       </section>
     </footer>
